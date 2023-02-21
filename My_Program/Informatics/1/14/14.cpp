99aec25ad37f3b5bc1ce0f7d0e0921e84ed270e3
Golub-Jora <sevavik40@gmail.com>
2023-02-21 17:50:24 +0700

    only for Michael Segreevich

--- My_Program/Informatics/1/14/14.cpp
+++
@@ -1,8 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-int main() {
-    int h, a, b;
-    cin >> h >> a >> b;
-    cout << (1 + (h - b - 1) / (a - b));
-    return 0;
-}
--- My_Program/Informatics/2/J/J.cpp
+++
@@ -1,9 +0,0 @@
-﻿#include <iostream>
-using namespace std;
-int main() {
-    int a, b, c, d;
-    cin >> a >> b >> c >> d;
-    if ((a + 1 == c || a - 1 == c || a == c) && (b + 1 == d || b - 1 == d || b == d) && 1) cout << "YES";
-    else cout << "NO";
-    return 0;
-}

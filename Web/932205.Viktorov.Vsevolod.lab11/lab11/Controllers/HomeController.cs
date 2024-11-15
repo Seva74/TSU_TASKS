70c8b27425bb9ee5ecf524d63ebea0fc5c30f141
Golub-Jora <sevavik40@gmail.com>
2024-11-16 00:23:41 +0700

    +lab11

---
+++ Web/932205.Viktorov.Vsevolod.lab11/lab11/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using lab11.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lab11.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ModelCalc()
+        {
+            CalculationModel model = new CalculationModel();   
+            return View(model);
+        }
+
+        public IActionResult ViewDataCalc()
+        {
+            CalculationModel model = new CalculationModel();
+            ViewData["firstNumber"] = model.firstNumber;
+            ViewData["secondNumber"] = model.secondNumber;
+            return View();
+        }
+
+        public IActionResult ViewBagCalc()
+        {
+            CalculationModel model = new CalculationModel();
+            ViewBag.firstNumber = model.firstNumber;
+            ViewBag.secondNumber = model.secondNumber;
+            return View();
+        }
+
+        public IActionResult InjectionCalc()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Web/932205.Viktorov.Vsevolod.lab11/lab11/Services/CalculationServiceImpl.cs
@@ -0,0 +1,35 @@
+﻿using lab11.Models;
+
+namespace lab11.Services
+{
+    public class CalculationServiceImpl : CalculationService
+    {
+        Random random = new Random();
+
+        public int getNumber()
+        {
+            return random.Next(10);
+        }
+
+        public string add(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        public string div(int a,int b)
+        {
+            if (b == 0) return "error";
+            else return (a / b).ToString();
+        }
+
+        public string mult(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+
+        public string sub(int a, int b)
+        {
+            return (a - b).ToString();
+        }
+    }
+}

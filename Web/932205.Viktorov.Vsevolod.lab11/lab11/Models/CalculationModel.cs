b904b0121851718b3780360cb7beb0be98ab889f
Golub-Jora <sevavik40@gmail.com>
2024-11-16 00:23:41 +0700

    +lab11

---
+++ Web/932205.Viktorov.Vsevolod.lab11/lab11/Models/CalculationModel.cs
@@ -0,0 +1,22 @@
+﻿namespace lab11.Models
+{
+    public class CalculationModel
+    {
+        public int firstNumber { get; set; }
+        public int secondNumber { get; set; }
+
+
+        public CalculationModel()
+        {
+            var rand = new Random(DateTime.Now.Millisecond);
+            firstNumber = rand.Next(10);
+            secondNumber = rand.Next(10);
+        }
+
+        public CalculationModel(int a, int b) 
+        {
+            firstNumber = a;
+            secondNumber = b;
+        }
+    }
+}
---
+++ Web/932205.Viktorov.Vsevolod.lab11/lab11/Services/CalculationService.cs
@@ -0,0 +1,15 @@
+﻿namespace lab11.Services
+{
+    public interface CalculationService
+    {
+        public int getNumber();
+
+        public string add(int a, int b);
+
+        public string div(int a, int b);
+
+        public string mult(int a, int b);
+
+        public string sub(int a, int b);
+    }
+}

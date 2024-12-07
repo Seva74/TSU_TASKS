3cbfe139fba3ed3d39cab28722d80d0dc7f26ceb
Golub-Jora <sevavik40@gmail.com>
2024-12-07 13:21:00 +0700

    Update script.sh

--- OS/lab3/script.sh
+++ OS/lab3/script.sh
@@ -7,6 +7,6 @@ echo "in the /proc/tsu file it is written:"
 cat /proc/tsu 
 echo "that means correct output"
 sudo rmmod tsulab
-dmesg | tail
+sudo dmesg | tail
 cat /proc/tsu
 rm tsulab.ko  tsulab.mod  tsulab.mod.c  tsulab.mod.o  tsulab.o modules.order Module.symvers

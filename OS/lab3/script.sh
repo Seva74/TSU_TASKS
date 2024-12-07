1f48c97b84b5b3951234a45329f09fd0c6d029b0
Golub-Jora <sevavik40@gmail.com>
2024-12-07 13:13:01 +0700

    +os_lab3

---
+++ OS/lab3/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+make
+sudo insmod tsulab.ko
+sudo dmesg | tail
+echo "in the /proc/tsu file it is written:" 
+cat /proc/tsu 
+echo "that means correct output"
+sudo rmmod tsulab
+dmesg | tail
+cat /proc/tsu
+rm tsulab.ko  tsulab.mod  tsulab.mod.c  tsulab.mod.o  tsulab.o modules.order Module.symvers

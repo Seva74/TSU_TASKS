56a32f9205119219d1763d02437cc2035592e021
Golub-Jora <sevavik40@gmail.com>
2025-05-14 23:56:44 +0700

    +4 lab

---
+++ Object-Oriented Analysis and Design/Lazy_Load/Without_Lazy_Load/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace NoLazyLoadDemo
+{
+    partial class Form1
+    {
+        private System.ComponentModel.Container components = null;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.Label headerLabel;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.headerPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(12, 66);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(300, 400);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(318, 66);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(670, 400);
+            this.webBrowser1.TabIndex = 1;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 469);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(0, 15);
+            this.statusLabel.TabIndex = 2;
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(120)))), ((int)(((byte)(215)))));
+            this.headerPanel.Controls.Add(this.headerLabel);
+            this.headerPanel.Location = new System.Drawing.Point(12, 12);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(976, 48);
+            this.headerPanel.TabIndex = 3;
+            this.headerPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.headerPanel_Paint);
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.headerLabel.ForeColor = System.Drawing.Color.White;
+            this.headerLabel.Location = new System.Drawing.Point(3, 9);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(184, 30);
+            this.headerLabel.TabIndex = 0;
+            this.headerLabel.Text = "Новостной Ридер";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 500);
+            this.Controls.Add(this.headerPanel);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Новостной ридер без ленивой загрузки";
+            this.headerPanel.ResumeLayout(false);
+            this.headerPanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+    }
+}
---
+++ Object-Oriented Analysis and Design/Lazy_Load/Without_Lazy_Load/Form1.cs
@@ -0,0 +1,103 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+namespace NoLazyLoadDemo
+{
+    public class NewsItem
+    {
+        public string Title { get; }
+        public string Filename { get; }
+        private readonly string content;
+
+        public NewsItem(string title, string filename)
+        {
+            Title = title;
+            Filename = filename;
+            Console.WriteLine($"Загрузка содержимого из {filename}...");
+            Thread.Sleep(2000);
+            try
+            {
+                content = File.ReadAllText(filename);
+            }
+            catch (Exception ex)
+            {
+                content = $"<html><body><h1>Ошибка загрузки</h1><p>{ex.Message}</p></body></html>";
+            }
+            Console.WriteLine($"Содержимое загружено из {filename}.");
+        }
+
+        public string GetContent()
+        {
+            return content;
+        }
+    }
+
+    public class NewsLibrary
+    {
+        private readonly List<NewsItem> newsItems;
+
+        public NewsLibrary()
+        {
+            newsItems = new List<NewsItem>
+            {
+                new NewsItem("Новые открытия в космосе", "news1.html"),
+                new NewsItem("Экономический прогноз на 2025 год", "news2.html"),
+                new NewsItem("Чемпионат мира по футболу", "news3.html"),
+            };
+        }
+
+        public List<NewsItem> GetNewsItems()
+        {
+            return newsItems;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        private readonly NewsLibrary newsLibrary;
+
+        public Form1()
+        {
+            InitializeComponent();
+            newsLibrary = new NewsLibrary();
+            listView1.Items.Clear();
+            foreach (var item in newsLibrary.GetNewsItems())
+            {
+                listView1.Items.Add(item.Title);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listView1.SelectedIndices.Count > 0)
+            {
+                int index = listView1.SelectedIndices[0];
+                var newsItem = newsLibrary.GetNewsItems()[index];
+                statusLabel.Text = "Отображение...";
+                Application.DoEvents();
+                string content = newsItem.GetContent();
+                webBrowser1.DocumentText = content;
+                statusLabel.Text = "";
+            }
+        }
+
+        private void headerPanel_Paint(object sender, PaintEventArgs e)
+        {
+            if (headerPanel.Width <= 0 || headerPanel.Height <= 0)
+                return;
+
+            using (var brush = new System.Drawing.Drawing2D.LinearGradientBrush(
+                headerPanel.ClientRectangle,
+                Color.FromArgb(0, 120, 215),
+                Color.FromArgb(0, 80, 180),
+                System.Drawing.Drawing2D.LinearGradientMode.Horizontal))
+            {
+                e.Graphics.FillRectangle(brush, headerPanel.ClientRectangle);
+            }
+        }
+    }
+}
